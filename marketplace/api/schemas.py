901dc8b9f41390d75acb7e5bd52b879b378f6028
samir984 <sasuki984@gmail.com>
2024-08-28 22:24:18 +0545

    too many issue

--- marketplace/api/schemas.py
+++ marketplace/api/schemas.py
@@ -1,19 +1,20 @@
-from pydantic import BaseModel
+from ninja import Schema
 from typing import Optional
 from datetime import datetime
+from django.contrib.auth.models import User
+from .models import Item,Bid,Auction
 
-class ItemSchema(BaseModel):
+
+class ItemSchema(Schema):
     id: int
     name: str
     description: str
     created_at: datetime
-    owner: User  
-   
+  
 
-    class Config:
-        orm_mode = True
 
-class AuctionSchema(BaseModel):
+
+class AuctionSchema(Schema):
     id: int
     item: int
     owner: int  
@@ -21,15 +22,13 @@ class AuctionSchema(BaseModel):
     start_time: datetime
     end_time: datetime
 
-    class Config:
-        orm_mode = True
+   
 
-class BidSchema(BaseModel):
+class BidSchema(Schema):
     id: int
     auction: int
     bidder_id: int 
     bid_amount: float
     created_at: datetime
 
-    class Config:
-        orm_mode = True
+  
