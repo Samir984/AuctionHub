f5322fd6d6afef027fd8161cb05dfc93b0cf3ee4
samir984 <sasuki984@gmail.com>
2024-08-28 09:35:26 +0545

    change mdel

--- marketplace/api/models.py
+++ marketplace/api/models.py
@@ -2,21 +2,23 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
+
 class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     starting_price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now) 
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='items')
+    image = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.name
+    
 
 class Auction(models.Model):
     item = models.OneToOneField(Item, on_delete=models.CASCADE)
     start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-
+    end_time = models.DateTimeField() 
     def is_active(self):
         now = timezone.now()
         return self.start_time <= now <= self.end_time
