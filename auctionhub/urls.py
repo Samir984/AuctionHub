166627418c41996c3d5afb0fe0c06decacd3b2bb
samir984 <sasuki984@gmail.com>
2024-10-21 22:24:15 +0545

    retryto build unfinished task

--- auctionhub/urls.py
+++ auctionhub/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for marketplace project.
+URL configuration for auctionhub project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -14,15 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("",views.home,name="website"),
-    path("api/",include("api.urls"))
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path("admin/", admin.site.urls),
+    path("api/", include("marketplace.urls")),
+]
